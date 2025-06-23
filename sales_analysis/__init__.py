9b1ab85926a294c84a960eea49061e6a0c76efd9
leesangmin4533 <kanura@naver.com>
2025-06-23 10:53:51 +0900

    Add sales detail extraction module and integrate

--- sales_analysis/__init__.py
+++ sales_analysis/__init__.py
@@ -2,6 +2,10 @@
 
 from .navigate_sales_ratio import navigate_sales_ratio
 from .sales_ratio_detail_extractor import extract_sales_ratio_details
+from .extract_sales_detail import extract_sales_detail
 
-__all__ = ["navigate_sales_ratio", "extract_sales_ratio_details"]
-
+__all__ = [
+    "navigate_sales_ratio",
+    "extract_sales_ratio_details",
+    "extract_sales_detail",
+]
